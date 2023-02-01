007abc22bc7d44cf637b3c9624263a6f6effb73a
Boshi <8356799+Boxis@users.noreply.github.com>
2023-01-31 19:02:19 -0500

    Add color change function

--- javascript.js
+++ javascript.js
@@ -1,18 +1,6 @@
 
 const container = document.querySelector('.container');
 
-// function createGrid(numBox) {
-//     for (let i = 0; i < numBox; i++) {
-//         const row = container.appendChild(document.createElement('div'));
-//         row.classList.add("box");
-//         for (let j = 0; j <numBox; j++) {
-//             const square = document.createElement('div');
-//             square.classList.add("box");
-//             row.appendChild(square);
-//         }
-//     }
-// }
-
 function createGrid(numBox) {
     let board = document.querySelector('.container');
     board.style.gridTemplateColumns=`repeat(${numBox}, 1fr)`;
@@ -24,3 +12,19 @@ function createGrid(numBox) {
 }
 
 createGrid(16);
+
+
+// Create object of boxes
+var elements = document.getElementsByClassName('box');
+var i;
+
+// console.log(elements);
+
+// Loop through every box and change color
+for (i = 0; i < elements.length; ++i) {
+ elements[i].addEventListener('mouseover', changeColor)  
+}
+
+function changeColor() {
+    this.style.backgroundColor = "black";
+}
